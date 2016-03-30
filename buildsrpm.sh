76e9e887781c1fae517d23e0dc59aa5feb8632bc
Andy Cobaugh <atc135@psu.edu>
2016-03-30 13:13:22 -0400

    Add support for SCMVERSION to buildsrpm.sh

--- buildsrpm.sh
+++ buildsrpm.sh
@@ -9,8 +9,17 @@ function fail {
 	fi
 }
 
-read VERSION RELEASE <<< $(git describe | perl -pe 's/v([0-9.]+)(-.*)?/$1/')
+read VERSION RELEASE SCMVERSION <<< $(git describe | perl -nle 'print "$1 $2 $3" if /v([0-9.]+)-?([0-9]+)?-?([0-9]+-.+)?/')
 fail $?
+
+echo VERSION = $VERSION
+echo RELEASE = $RELEASE
+echo SCMVERSION = $SCMVERSION
+
+# prepend a dot if SCMVERSION is not empty
+if [ -n "$SCMVERSION" ] ; then
+	SCMVERSION=".$(echo $SCMVERSION | sed -e 's/-/_/g')"
+fi
 
 tmp=$(mktemp -d /tmp/rt-rpm-XXXX)
 echo Temporary directory is $tmp
@@ -23,7 +32,7 @@ echo Creating tarball of shipyard
 tar -czf $tmp/SOURCES/rt-shipyard.tar.gz rt-shipyard
 fail $?
 
-cat rt.spec | sed -e "s/__VERSION__/$VERSION/;s/__RELEASE__/$RELEASE/" > $tmp/SPECS/rt.spec
+cat rt.spec | sed -e "s/__VERSION__/$VERSION/;s/__RELEASE__/$RELEASE/;s/__SCMVERSION__/$SCMVERSION/" > $tmp/SPECS/rt.spec
 fail $?
 
 echo Building SRPM
